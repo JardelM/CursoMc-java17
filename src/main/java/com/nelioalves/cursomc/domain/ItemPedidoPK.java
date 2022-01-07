808586b35901b4ea98f4d910fbbd81a8155c686c
Jardel Morais <moraiss.jardel@gmail.com>
2022-01-07 20:25:23 -0300

    endpoint /pedidos/{id} disponivel

--- src/main/java/com/nelioalves/cursomc/domain/ItemPedidoPK.java
+++ src/main/java/com/nelioalves/cursomc/domain/ItemPedidoPK.java
@@ -13,6 +13,7 @@ public class ItemPedidoPK implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
 	
+	
 	@ManyToOne
 	@JoinColumn(name = "pedido_id")
 	private Pedido pedido;
